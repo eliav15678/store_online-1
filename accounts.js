d6e7bd61d68fd6f23057bd0e012d168f1ea3a0be
eliav15678 <eliav15678@gmail.com>
2022-04-25 13:34:13 +0300

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,24 +1,40 @@
 import express from 'express';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 const router = express.Router();
+
+
 let users= [];
+
+
+
 router.post('/login', (req,res) => {
-
-    //1
-    const fn = req.body.name;
-    const ln = req.body.lastname;
-
-    //2
-    const {name,lastname} = req.body;
-
-    return res.status(200).json({
-        message: `Hello ${name} ${lastname}`
-    })
+    const {email,password}=req.body;
+    const find= users.find(x => x.email == email)
+    if (find)
+    {
+        const match = await bcryptjs.compare(password,find.password);
+        if (match)
+        {
+            return res.status(200).json({
+                message:`hello ${find.name}`
+            })
+        }
+        else
+        {
+            return res.status(200).json({
+                message:`password not match`
+            })
+        }
+    }
 })
+
+
 
 router.post('/register', async(req,res) => {
     // const name = req.body.fname +' '+ req.body.lname;
     // const email=req.body.email;
-    const {fname,lname,email}=req.body;
+    const {fname,lname,email,password}=req.body;
     const check = users.filter(x => x.email==email)
     if (check.length > 0)
     {
@@ -26,7 +42,10 @@ router.post('/register', async(req,res) => {
              message: 'the email in use'
         })
     }else{
-    const user ={fname:fname,lname:lname,email:email}
+
+        const hash = await bcryptjs.hash(password,10)
+
+    const user ={fname:fname,lname:lname,email:email,password:hash}
     users.push(
         user
     )
@@ -35,4 +54,6 @@ router.post('/register', async(req,res) => {
     })
 }
 })
+
+
 export default router;
